6c6800ae01f2668934b05e60bf059a366e5deff8
Daria <ddenisova@skolopendra.com>
2022-06-28 20:37:27 +0300

    Judge model

---
+++ lib/infrastructure/data/models/judge_model.dart
@@ -0,0 +1,33 @@
+class JudgeModel {
+  int id;
+  String firstName;
+  String lastName;
+  String thirdName;
+  String avatarUrl;
+  String region;
+  String city;
+  String judgeCategory;
+  String organization;
+
+  JudgeModel({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.thirdName,
+    required this.avatarUrl,
+    required this.city,
+    required this.region,
+    required this.judgeCategory,
+    required this.organization,
+  });
+
+  @override
+  String toString() => "JudgeModel. id: $id"
+      "firstName: $firstName"
+      "lastName: $lastName"
+      "thirdName: $thirdName"
+      "avatarUrl: $avatarUrl"
+      "region: $region"
+      "city: $city"
+      "organization: $organization.";
+}
